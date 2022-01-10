12af9d2b0dc0f5d6d57e251ffbf9267f97f4fb2e
Andrea Boccardo <andrea.boccardo@gmail.com>
2022-01-10 15:54:35 -0300

    Prueba del curso Programación Básica en Java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,7 @@ import vista.Menu;
 public class Main {
 
 	public static void main(String[] args) {
-		//ruta C:\Users\mikaz\eclipse-workspace\SistemaClientes\src\main\java\clientes.csv
+		//ruta C:\Users\mikaz\eclipse-workspace\SistemaClientes\DBClientes.csv
 		Menu menu = new Menu();
 		menu.iniciarMenu();
 	}
